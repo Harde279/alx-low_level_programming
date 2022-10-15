deb9189a4cbc03c690a2632d0a07d675a2d75af7
Harde279 <ibrahimadebisi279@gmail.com>
2022-10-15 01:36:48 -0700

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - print alphabets in lowercase
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;	
+	}
+	putchar('\n');
+	return (0);
+}
