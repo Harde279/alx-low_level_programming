642b96dc7dd0ae8cfbce5d4f9dd48d3ac2375d8c
Harde279 <ibrahimadebisi279@gmail.com>
2022-10-18 09:18:51 -0700

    .

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*
+*main - Entry point
+*
+*Description: Print putchar followed by a new line
+*Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+
+	char s[] = "_putchar";
+
+	for(i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Functions and nested loops
+Task 1: Print putchar followed by a new line
