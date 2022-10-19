3da7d8bc14827cf6d3e5ebcc02e39dc16be5e2e8
Harde279 <ibrahimadebisi279@gmail.com>
2022-10-19 07:45:49 -0700

    .

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* add - add two numbers
+* @n: first parameter
+* @m: second parameter
+* Return: Always 0
+*/
+
+int add(int n, int m)
+{
+	int sum = 0;
+
+	sum = n + m;
+	return (sum);
+}
