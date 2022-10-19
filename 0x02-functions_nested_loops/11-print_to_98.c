3ffcace4bd66c8f4cd886b95c28b569c2ddd5573
Harde279 <ibrahimadebisi279@gmail.com>
2022-10-19 09:42:10 -0700

    .

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,18 +13,18 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d\n", n);
+			printf("%d, ", n);
 		}
 	}
 	else if (n < 98)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d\n", n);
+			printf("%d, ", n);
 		}
 	}
 	else
 	{
-		printf("%d\n", n);
+		printf("%d, ", n);
 	}
 }
