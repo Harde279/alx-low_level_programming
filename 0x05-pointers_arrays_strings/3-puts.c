5edcbe73df57283c7498b073970327fbbe9748f4
Harde279 <Ibrahimadebisi279@gmail.com>
2022-10-24 17:10:10 -0400

    .

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - prints string with terminating null byte
+* @str: parameter to be printed
+*/
+
+void _puts(char *str)
+{
+	for (int a  = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
