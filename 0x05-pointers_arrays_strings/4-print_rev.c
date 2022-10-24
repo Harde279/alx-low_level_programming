610d8ed1737615e38799a8a5a81b87e71c1e80cb
Harde279 <Ibrahimadebisi279@gmail.com>
2022-10-24 17:56:50 -0400

    .

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_rev - print the reverse of the string passed
+* @s: the parameter passed to the function
+*/
+
+void print_rev(char *s)
+{
+	int n, sum;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		sum += 1;
+	}
+	for (n <= sum; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+
+}
