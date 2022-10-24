6022055331409ed921d3cb55c602c200c1af9efe
Harde279 <Ibrahimadebisi279@gmail.com>
2022-10-24 16:21:59 -0400

    .

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* swap_int - swap two integers using the pointers pointing to them
+*@a: first integer to be swapped
+*@b: second integer to be swapped
+*Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *b;
+	*b = *a;
+	*a = c;
+}
