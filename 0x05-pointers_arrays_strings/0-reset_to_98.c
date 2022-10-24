1717461f49dc82a24b5fc5af315d8f1868618dca
Harde279 <Ibrahimadebisi279@gmail.com>
2022-10-24 15:41:21 -0400

    .

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* reset_to_98 - set the value of the integer to 98
+* @n: integer value that is to be set
+* Return: Always 0
+*/
+
+void reset_to_98(int *n)
+{
+	int *m;
+
+	m = n;
+
+	*m = 98;
+}
