4446b7ca77794e1c983404edb548ca5b860613a1
Harde279 <Ibrahimadebisi279@gmail.com>
2022-11-18 14:55:18 -0500

    .

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - check the code for Holberton School students.
+* @argc: argument count.
+* @argv: argument vector.
+*
+* Return: Always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
